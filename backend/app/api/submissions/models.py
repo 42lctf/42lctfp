b1425dcbf8e2fd72c797cbb6ef2374aa6730a9ae
Esanchez <esanchez@student.42lausanne.ch>
2024-01-05 15:05:48 +0100

    fixes for review part 3

--- backend/app/api/submissions/models.py
+++ backend/app/api/submissions/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class Submission(Base):
     __tablename__ = "submissions"
     id = Column(UUID(as_uuid=True), primary_key=True, index=True)
-    challenge_id = Column(UUID(as_uuid=True), ForeignKey("challenge.id"), nullable=False)
+    challenge_id = Column(UUID(as_uuid=True), ForeignKey("challenges.id"), nullable=False)
     user_id = Column(UUID(as_uuid=True), ForeignKey("ctf_users.id"), nullable=False)
     content = Column(String(100), nullable=False)
     ip = Column(String(46), nullable=False)
@@ -20,7 +20,7 @@ class Submission(Base):
 class Solve(Base):
     __tablename__ = "solves"
     id = Column(UUID(as_uuid=True), primary_key=True, index=True)
-    challenge_id = Column(UUID(as_uuid=True), ForeignKey("challenge.id"), nullable=True)
+    challenge_id = Column(UUID(as_uuid=True), ForeignKey("challenges.id"), nullable=True)
     user_id = Column(UUID(as_uuid=True), ForeignKey("ctf_users.id"), nullable=False)
     created_at = Column(DateTime(), default=datetime.now(), nullable=False)
     updated_at = Column(DateTime(), default=datetime.now(), nullable=False)
