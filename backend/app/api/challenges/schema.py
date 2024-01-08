f18e7fefbcad75b9348853ca0bde77acc8aa270e
lenart <lenartlola@proton.me>
2024-01-08 23:14:11 +0100

    Add CreateChallengeRequest

--- backend/app/api/challenges/schema.py
+++ backend/app/api/challenges/schema.py
@@ -1,1 +1,18 @@
+from datetime import datetime
+
 from pydantic import BaseModel
+
+
+class CreateChallengeRequest(BaseModel):
+    title: str
+    description: str
+    value: int
+    is_hidden: bool = True
+    flag: str
+    flag_case_sensitive: bool = False
+    challenge_type: str = 'NORMAL'
+    difficulty_id: str
+    parent_id: str
+    category_id: str
+    created_at: datetime
+    updated_at: datetime
