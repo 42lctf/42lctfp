5942f5d4151285d3f6164c84fdb795dc44a274f4
Esanchez <esanchez@student.42lausanne.ch>
2024-01-11 14:17:40 +0100

    add patch and delete fordifficulties endpoints

--- backend/app/api/difficulties/endpoints.py
+++ backend/app/api/difficulties/endpoints.py
@@ -2,15 +2,26 @@ from typing import Annotated, Union
 
 from fastapi import APIRouter, status, Cookie, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy.dialects.postgresql import UUID
 
 from .schemas import CreateNewDifficultyRequest
-from .services import crete_new_difficulty
+from .services import create_new_difficulty, update_difficulty, delete_difficulty
 from app.db import get_session
 
 DifficultyRouter = APIRouter()
 
 
-@DifficultyRouter.post('/difficulties/new', status_code=status.HTTP_201_CREATED)
+@DifficultyRouter.post('/difficulties', status_code=status.HTTP_201_CREATED)
 async def create_difficulty(body: CreateNewDifficultyRequest, access_token: Annotated[Union[str, None], Cookie()] = None, db: Session = Depends(get_session)):
-    crete_new_difficulty(body, access_token, db)
+    create_new_difficulty(body, access_token, db)
     return {"message": "New difficulty created successfully"}
+
+@DifficultyRouter.patch('/difficulties/{difficulty_id}', status_code=status.HTTP_200_OK)
+async def update_difficulty_request(difficulty_id: str, body: CreateNewDifficultyRequest, access_token: Annotated[Union[str, None], Cookie()] = None, db: Session = Depends(get_session)):
+    update_difficulty(difficulty_id, body, access_token, db)
+    return {"message": "Difficulty successfully modified"}
+
+@DifficultyRouter.delete('/difficulties/{difficulty_id}', status_code=204)
+async def delete_difficulty_request(difficulty_id: str, access_token: Annotated[Union[str, None], Cookie()] = None, db: Session = Depends(get_session)):
+    delete_difficulty(difficulty_id, access_token, db)
+    return {"message": "Difficulty successfully modified"}
--- backend/app/api/difficulties/services.py
+++ backend/app/api/difficulties/services.py
@@ -1,15 +1,18 @@
 from uuid import uuid4
+from datetime import datetime
 
 from fastapi import HTTPException, status
 
 from sqlalchemy.orm import Session
+from sqlalchemy.dialects.postgresql import UUID
 
 from ..users.general_utils import get_user_by_token
 from .schemas import CreateNewDifficultyRequest
 from .models import Difficulty
+from app.api.challenges.models import Challenge
 
 
-def crete_new_difficulty(body: CreateNewDifficultyRequest, access_token: str, db: Session):
+def create_new_difficulty(body: CreateNewDifficultyRequest, access_token: str, db: Session):
     user = get_user_by_token(access_token, db)
     if not user.is_user_admin():
         raise HTTPException(
@@ -37,3 +40,63 @@ def crete_new_difficulty(body: CreateNewDifficultyRequest, access_token: str, db
     db.add(difficulty)
     db.commit()
     db.refresh(difficulty)
+
+
+def update_difficulty(difficulty_id: str, body: CreateNewDifficultyRequest, access_token: str, db: Session):
+    user = get_user_by_token(access_token, db)
+    if not user.is_user_admin():
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Only admin can update difficulty"
+        )
+    difficulty = db.query(Difficulty).filter(Difficulty.id == difficulty_id).first()
+    if difficulty is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Difficulty not found"
+        )
+    difficulty_name = db.query(Difficulty).filter(Difficulty.name == body.name, Difficulty.id != difficulty_id).first()
+    if difficulty_name:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Difficulty name already exists"
+        )
+    if len(body.name) > 50:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Difficulty name too long"
+        )
+
+    difficulty.level = body.level
+    difficulty.name = body.name
+    difficulty.updated_at = datetime.utcnow()
+
+    db.commit()
+    db.refresh(difficulty)
+
+
+def delete_difficulty(difficulty_id: str, access_token: str, db: Session):
+    user = get_user_by_token(access_token, db)
+    if not user.is_user_admin():
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Only admin can delete difficulty"
+        )
+    difficulty = db.query(Difficulty).filter(Difficulty.id == difficulty_id).first()
+    if difficulty is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Difficulty not found"
+        )
+    challenges_using_difficulty = db.query(Challenge).filter(Challenge.difficulty_id == difficulty_id).all()
+    if challenges_using_difficulty:
+        challenge_names = [challenge.title for challenge in challenges_using_difficulty]
+        challenges_str = ', '.join(challenge_names)
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Difficulty is used by challenges: {challenges_str}. Cannot delete."
+        )
+
+    db.delete(difficulty)
+    db.commit()
+    
